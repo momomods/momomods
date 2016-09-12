5c239025fd7bed293ce121f8340bb71cf58cfcf2
patcho <patcho168@gmail.com>
2016-09-12 16:29:41 +0800

    make year into 2016-2017 instead of 2016 for bootstrap-dev-database.sql

--- bootstrap-dev-database.sql
+++ bootstrap-dev-database.sql
@@ -4,11 +4,11 @@ VALUES ("Patrick Cho", "patcho168@gmail.com", 0, CURRENT_TIMESTAMP, CURRENT_TIME
 
 -- Insert Timetables
 INSERT INTO Timetable (userId, year, semester, createdAt, updatedAt)
-VALUES (1, 2016, 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+VALUES (1, "2016-2017", 1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 -- Insert Modules
 INSERT INTO Module (year, semester, code, title, description, department, credit, workload, prerequisite,
-preclusion, examDate, timetable, createdAt, updatedAt) VALUES (2016, 1, "CS3216", "Software Product Engineering for Digital Markets",
+preclusion, examDate, timetable, createdAt, updatedAt) VALUES ("2016-2017", 1, "CS3216", "Software Product Engineering for Digital Markets",
 "In this module, students will practice software product engineering by working in small teams to develop well-tested, user-friendly, production-quality software for the real world. To support this goal, students work closely with users to understand their problems, gather their requirements, and obtain their feedback through a rapid, iterative, application design and development process. Students will also be exposed to practical issues for digital markets such as growing the user base of their application, deployment of the application on the Web or in the cloud system, and validating the UI design and UX of the application.",
 "Computer Science", 5, "2-1-0-8-2", "CS2103/CS2103T or with special approval from instructor. Students will submit personal statements to apply for a place in the course instead of bidding through the CORS system.",
 null, null, '[{"ClassNo":"1","LessonType":"Lecture","WeekText":"Every Week","DayText":"Monday","StartTime":"1830","EndTime":"2030","Venue":"VCRm"}],"LecturePeriods":["Monday Evening"]',
