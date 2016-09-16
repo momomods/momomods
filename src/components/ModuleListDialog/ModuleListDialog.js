2cca0d28bfa3bc1b42a3a3d5304341fb9d16e78a
Ng Zhi An <ngzhian@gmail.com>
2016-09-16 15:53:16 +0800

    Clean up ModuleListDialog

--- src/components/ModuleListDialog/ModuleListDialog.js
+++ src/components/ModuleListDialog/ModuleListDialog.js
@@ -9,47 +9,71 @@ import { lightGreen500 } from 'material-ui/styles/colors';
 
 import s from './ModuleListDialog.css';
 
+/*
+ * This is a Dialog that shows module information.
+ *
+ * Requires a module, a param to indicate if dialog is open,
+ * and handlers for tapping the close button, and add to timetable button.
+ */
 class ModuleListDialog extends Component {
+  displayName = 'ModuleListDialog'
+
+  handleAdd = () => this.props.handleAddToTimetable(this.props.module)
 
   render() {
+    let {
+      handleAddToTimetable,
+      handleClose,
+      module,
+      open,
+    } = this.props;
+    const title = `${module.code} ${module.title}`;
+
+    let actions = []
+    if (handleAddToTimetable) {
+      actions = [
+        <FlatButton
+          label="Add to Timetable"
+          primary
+          onTouchTap={() => handleAddToTimetable(module)}
+        />];
+    }
+
     return (
-        <Dialog
-          title={this.props.module.code + ' ' + this.props.module.title}
-          actions={<FlatButton
-                label="Add to Timetable"
-                primary
-                onTouchTap={() => this.props.handleAddToTimetable(this.props.module)}
-              />}
-          modal={false}
-          open={this.props.open}
-          onRequestClose={this.props.handleClose}
-          autoScrollBodyContent
-        >
-          <div>
-            <h3>Description</h3>
-            <p>{this.props.module.description}</p>
-            <h3>Semesters Offered</h3>
-            <p>{this.props.module.semester}</p>
-            <h3>Modular Credits</h3>
-            <p>{this.props.module.credit} MCs</p>
-            <h3>Preclusions</h3>
-            <p>{this.props.module.preclusion}</p>
-            <h3>Department</h3>
-            <p>{this.props.module.department}</p>
-            <h3>Weekly Workload</h3>
-            <p>{this.props.module.workload}</p>
-            <h3>Exam Dates</h3>
-            <p>Semester {this.props.module.semester}: {this.props.module.examDate}</p>
-          </div>
-        </Dialog>
+      <Dialog
+        title={title}
+        actions={actions}
+        modal={false}
+        open={open}
+        onRequestClose={handleClose}
+        autoScrollBodyContent
+      >
+        <div>
+          <h3>Description</h3>
+          <p>{module.description}</p>
+          <h3>Semesters Offered</h3>
+          <p>{module.semester}</p>
+          <h3>Modular Credits</h3>
+          <p>{module.credit} MCs</p>
+          <h3>Preclusions</h3>
+          <p>{module.preclusion}</p>
+          <h3>Department</h3>
+          <p>{module.department}</p>
+          <h3>Weekly Workload</h3>
+          <p>{module.workload}</p>
+          <h3>Exam Dates</h3>
+          <p>Semester {module.semester}: {module.examDate}</p>
+        </div>
+      </Dialog>
     );
   }
 }
 
 ModuleListDialog.propTypes = {
-  module: PropTypes.object.isRequired,
-  handleAddToTimetable: PropTypes.func.isRequired,
+  handleAddToTimetable: PropTypes.func,
   handleClose: PropTypes.func.isRequired,
+  module: PropTypes.object.isRequired,
+  open: PropTypes.bool.isRequired,
 };
 
 export default withStyles(s)(ModuleListDialog);
