5fa2f1010c2b0f927220efaab206ed906f6fffae
Ng Zhi An <ngzhian@gmail.com>
2016-09-17 17:43:34 +0800

    Clicking on login navigates to login page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,34 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 
 import Navigation from '../Navigation';
+import Link from '../Link';
+import { navigate } from '../../actions/route';
 
 import s from './Header.css';
 
 class Header extends Component {
   render() {
+    const loginButton = (
+      <FlatButton
+        label="Login"
+        onTouchTap={() => this.props.navigate("/login")}
+      />
+    )
     return (
       <div>
         <div style={{ position: 'fixed', width: '100%', zIndex: 10 }}>
           <AppBar
             title="mods+"
             showMenuIconButton={false}
-            iconElementRight={<FlatButton label="Login" />}
+            iconElementRight={loginButton}
             zDepth={0}
           />
-          <Navigation activeTab={this.props.activeTab} />
+          <Navigation activeTab={this.props.activeTab || "mods+"} />
         </div>
         <div style={{ height: '112px', width: '100%' }} />
       </div>
@@ -27,4 +36,8 @@ class Header extends Component {
   }
 }
 
-export default withStyles(s)(Header);
+Header.propTypes = {
+  activeTab: PropTypes.string.isRequired,
+}
+
+export default connect(null, { navigate })(withStyles(s)(Header));
