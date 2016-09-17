9258972081167f96075a1c274c90594f435a996e
Nicholette <nekonekonik@gmail.com>
2016-09-17 22:39:25 +0800

    Account for 6am classes

--- src/components/Timetable/TimeRow.js
+++ src/components/Timetable/TimeRow.js
@@ -4,9 +4,10 @@ import { FIRST_HOUR, LAST_HOUR } from '../../utils/timetable';
 import TimetableCell from './TimetableCell';
 import s from './timetable.scss';
 
-const TIME = ['0800', '0900', '1000', '1100', '1200', '1300',
-              '1400', '1500', '1600', '1700', '1800', '1900',
-              '2000', '2100', '2200', '2300', '2359'];
+const TIME = ['0600', '0700', '0800', '0900', '1000', 
+              '1100', '1200', '1300', '1400', '1500',
+              '1600', '1700', '1800', '1900', '2000', 
+              '2100', '2200', '2300', '2359'];
 
 const generateCells = () => {
   const cells = [];
