6b49e88931ee0db4e6ae5cde7dfaa732bab3059a
Ng Zhi An <ngzhian@gmail.com>
2016-09-18 23:59:30 +0800

    Do not specify defult facebook id and secret

--- src/config.js
+++ src/config.js
@@ -29,7 +29,7 @@ export const auth = {
 
   // https://developers.facebook.com/
   facebook: {
-    id: process.env.FACEBOOK_APP_ID || '186244551745631',
-    secret: process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+    id: process.env.FACEBOOK_APP_ID,
+    secret: process.env.FACEBOOK_APP_SECRET,
   },
 };
