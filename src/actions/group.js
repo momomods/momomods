de46f6ff22d8fa40df3d4e28bd37eba8d92ceb2e
Nicholette <nekonekonik@gmail.com>
2016-09-20 07:05:56 +0800

    Add group actions

--- src/actions/group.js
+++ src/actions/group.js
@@ -13,10 +13,59 @@ import {
  * @param {string} semester, "1", "2", etc.
  */
 export function fetchGroups({ year, semester }) {
+  const url = `/api/${year}/${semester}/team`;
   return {
     type: FETCH_GROUPS,
+    meta: {
+      year,
+      semester,
+    },
+    payload: {
+      promise: request(url),
+    },
+  };
+}
+
+export function addGroup({ year, semester, teamName}) {
+  const url = `/api/${year}/${semester}/team`;
+  return {
+    type: ADD_GROUP,
+    meta: {
+      year,
+      semester,
+    },
+    payload: {
+      promise: postRequest(url, {
+        body: JSON.stringify(teamName),
+      }),
+    },
+  };
+}
+
+// TODO: Add/ delete friends?
+export function editGroup({ year, semester }) {
+  return {
+    type: EDIT_GROUP,
     payload: {
       promise: Promise.resolve({ year, semester, data: [] }),
+    },
+  };
+}
+
+/**
+ * Select the group to be displayed
+ *
+ * @param {string} year, in the format of "YYYY-YYYY"
+ * @param {string} semester, "1", "2", etc.
+ * @param {string} groupId, id of the group to be displayed
+ */
+export function changeGroup({ year, semester, groupId}) {
+  return {
+    type: CHANGE_GROUP,
+    payload: {
+      year,
+      semester,
+      groupId,
     },
   };
 }
