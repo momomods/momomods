1db1ba2637e72b865ee65257509d98a819d76b70
Ng Zhi An <ngzhian@gmail.com>
2016-09-21 13:28:52 +0800

    Cache group.png

--- src/service-worker.js
+++ src/service-worker.js
@@ -4,13 +4,14 @@ var cacheName = 'modsplus-1';
 importScripts('/sw-toolbox.js');
 
 // toolbox.options.debug = true;
-toolbox.precache(['/manifest.json', '/assets/main.js']);
+toolbox.precache(['/manifest.json', '/assets/main.js', '/group.png']);
 
 // TODO: think about how we want to handle other routes
 // toolbox.router.default = toolbox.networkOnly;
 
 // we use query busting to fetch new main.js
 toolbox.router.get('/assets/main.js', toolbox.cacheFirst);
+toolbox.router.get('/group.png', toolbox.cacheFirst);
 
 toolbox.router.get('/manifest.json', toolbox.networkFirst);
 toolbox.router.get('/graphql', toolbox.networkFirst);
