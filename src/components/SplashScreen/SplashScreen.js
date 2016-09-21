81f846a5ec572d070b1d4c65584a8e9b170e0459
Ng Zhi An <ngzhian@gmail.com>
2016-09-21 15:45:45 +0800

    Lint fix

--- src/components/SplashScreen/SplashScreen.js
+++ src/components/SplashScreen/SplashScreen.js
@@ -4,16 +4,16 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './SplashScreen.css';
 
 class SplashScreen extends Component {
-    render() {
-        return (
-            <div className={s.splashContainer}>
-                <div className={s.logoContainer}>
-                    <img src="/logo_colour_300.png" />
-                </div>
-                <div className={s.textContainer}>mods+</div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className={s.splashContainer}>
+        <div className={s.logoContainer}>
+          <img src="/logo_colour_300.png" />
+        </div>
+        <div className={s.textContainer}>mods+</div>
+      </div>
+    );
+  }
 }
 
 export default withStyles(s)(SplashScreen);
