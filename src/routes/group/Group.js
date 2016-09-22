61f57f19b5365794b44a98faf26e4809a313b06b
Nicholette <nekonekonik@gmail.com>
2016-09-22 19:39:10 +0800

    Pad date with leading zero if single digit

--- src/routes/group/Group.js
+++ src/routes/group/Group.js
@@ -58,7 +58,7 @@ class Group extends Component {
   }
 
   formatDate(date) {
-    return date.getFullYear() + '-' +  ('00' + (date.getMonth() + 1)).slice(-2) + '-' + date.getDate();
+    return date.getFullYear() + '-' +  ('00' + (date.getMonth() + 1)).slice(-2) + '-' + ('00' + date.getDate()).slice(-2);
   }
 
   handleGroupChange = (event, key, groupId) => {
