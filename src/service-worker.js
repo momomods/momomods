3c8db44f24784ec23b69f1b3a681d7fdbe91b7dc
Ng Zhi An <ngzhian@gmail.com>
2016-09-23 13:57:40 +0800

    Update service worker to serve production js files

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,24 +1,29 @@
-/* eslint-disable */
+// /* eslint-disable */
 importScripts('/sw-toolbox.js');
 
-// toolbox.options.debug = true;
+toolbox.options.debug = true;
 toolbox.precache([
   '/manifest.json',
-  '/assets/main.js',
   '/group.png',
+  '/facebook.png',
   '/logo_colour_300.png',
 ]);
 
-// TODO: think about how we want to handle other routes
-// toolbox.router.default = toolbox.networkOnly;
+// // TODO: think about how we want to handle other routes
+toolbox.router.default = toolbox.networkOnly;
 
-// we use query busting to fetch new main.js
+// // we use query busting to fetch new main.js
 toolbox.router.get('/assets/main.js', toolbox.cacheFirst);
+toolbox.router.get(/assets\/main.[0-9a-zA-Z]+.js$/, toolbox.cacheFirst);
+// // https://modspl.us/assets/main.058f4d622a6fb2e6a846.js
 toolbox.router.get('/group.png', toolbox.cacheFirst);
+toolbox.router.get('/facebook.png', toolbox.cacheFirst);
 toolbox.router.get('/logo_colour_300.png', toolbox.cacheFirst);
 
 toolbox.router.get('/manifest.json', toolbox.networkFirst);
-toolbox.router.get('/graphql', toolbox.networkFirst);
+toolbox.router.get(/graphql/, toolbox.networkFirst);
 toolbox.router.get('/api', toolbox.networkFirst);
 toolbox.router.get('/', toolbox.networkFirst);
+toolbox.router.get('/module', toolbox.networkFirst);
+toolbox.router.get('/group', toolbox.networkFirst);
 // /* eslint-enable */
